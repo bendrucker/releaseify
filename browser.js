1dddfea975ee5747f8a9a6d4d587b049e966299d
Ben Drucker <bvdrucker@gmail.com>
2016-03-16 22:45:02 -0700

    Rename noGit fn

--- browser.js
+++ browser.js
@@ -2,9 +2,9 @@
 
 var nextTick = require('next-tick')
 
-module.exports = noopGit
+module.exports = noGit
 
-function noopGit (callback) {
+function noGit (callback) {
   nextTick(function () {
     callback(new Error('git data is not available in the browser without the browserify transform'))
   })
